135537f24fbd8e70f1e2424fb47fb0b993bcde9f
shubham14p3 <shubham14p3@gmail.com>
2020-04-12 19:41:54 +0530

    Watch condition added

--- lib/match.rb
+++ lib/match.rb
@@ -17,5 +17,16 @@ class Match
       user_move
       @board.display
       case @board.board_completed(@current_player)
-     
+      when 1
+        @user_interface.output_message("Congratulations player #{@current_player.id}, you won!")
+        match_finished = true
+        @current_player.score += 1
+      when 2
+        @user_interface.output_message('Match draw!')
+        match_finished = true
+      when 0
+        @current_player = @current_player == @players[0] ? @players[1] : @players[0]
+      end
+    end
+  end
 end
