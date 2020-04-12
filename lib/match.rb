35336db74dd047c0ea785377e1c040879c3f4003
shubham14p3 <shubham14p3@gmail.com>
2020-04-12 19:41:37 +0530

    Watch test cases added

--- lib/match.rb
+++ lib/match.rb
@@ -10,5 +10,12 @@ class Match
 
   public
 
-  
+  def start
+    match_finished = false
+    @board.display
+    until match_finished
+      user_move
+      @board.display
+      case @board.board_completed(@current_player)
+     
 end
