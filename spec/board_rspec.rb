ebed3686b87284f6fdf36dc2ea23107550910a5f
shubham14p3 <shubham14p3@gmail.com>
2020-04-14 18:39:27 +0530

    apply mode added

--- spec/board_rspec.rb
+++ spec/board_rspec.rb
@@ -11,5 +11,18 @@ describe Board do
     Board.new(my_ui)
   end
 
- 
+  describe '#apply_move?' do
+    it 'should apply the move to the specified position' do
+      my_board.apply_move?(my_player.character, 1)
+      expect(my_board.cells[0][0]).to eql('X')
+    end
+
+    it 'should return false if the position is already taken' do
+      my_board.apply_move?(my_player.character, 1)
+      expect(
+        my_board.apply_move?(my_player.character, 1)
+      ).not_to eql(true)
+    end
+  end
+
 end
