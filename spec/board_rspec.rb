39ed26579e0f137cf1ca8848e5b70c3dd5b0d27a
shubham14p3 <shubham14p3@gmail.com>
2020-04-14 18:40:01 +0530

    board completed move  added

--- spec/board_rspec.rb
+++ spec/board_rspec.rb
@@ -25,4 +25,49 @@ describe Board do
     end
   end
 
+  describe '#board_completed' do
+    it 'should return true when there is a win state on the board - top row' do
+      my_board.apply_move?(my_player.character, 1)
+      my_board.apply_move?(my_player.character, 2)
+      my_board.apply_move?(my_player.character, 3)
+      expect(
+        my_board.board_completed(my_player)
+      ).to eql(1)
+    end
+
+    it 'should return true when there is a win state on the board - main diagonal' do
+      my_board.apply_move?(my_player.character, 1)
+      my_board.apply_move?(my_player.character, 5)
+      my_board.apply_move?(my_player.character, 9)
+      expect(
+        my_board.board_completed(my_player)
+      ).to eql(1)
+    end
+
+    it 'should return 1 when there is a win state on the board - first column' do
+      my_board.apply_move?(my_player.character, 1)
+      my_board.apply_move?(my_player.character, 4)
+      my_board.apply_move?(my_player.character, 7)
+      expect(
+        my_board.board_completed(my_player)
+      ).to eql(1)
+    end
+
+    it 'should return 2 if there is a drawn match' do
+      # Each line represents a move
+      my_board.apply_move?(my_player.character, 1)
+      my_board.apply_move?(my_player.character, 2)
+      my_board.apply_move?('O', 3)
+      my_board.apply_move?('O', 4)
+      my_board.apply_move?('O', 5)
+      my_board.apply_move?(my_player.character, 6)
+      my_board.apply_move?(my_player.character, 7)
+      my_board.apply_move?(my_player.character, 8)
+      my_board.apply_move?('O', 9)
+      expect(
+        my_board.board_completed(my_player)
+      ).to eql(2)
+    end
+  end
+
 end
